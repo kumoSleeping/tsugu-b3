1e19af450cc0138e4078dc3cb36f9fe5f9b98215
kumoSleeping <zjr2992@outlook.com>
2025-07-24 18:06:31 +0800

    发版

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='tsugu',
-    version='6.1.5',
+    version='6.2.0',
     author='kumoSleeping',
     author_email='zjr2992@outlook.com',
     license="MIT",
